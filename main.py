d397a3304e3fbbf1bd22a0b8b0a24c180886ceb0
CoryOConnor94 <coryoconnor1994@gmail.com>
2024-09-24 20:41:35 +0100

    Updated UI

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import yt_dlp
+from datetime import datetime as dt
 from tkinter import *
 from tkinter import messagebox
 
+# Get current time and date
+now = dt.now()
+formatted_time = now.strftime("%Y-%m-%d_%H-%M-%S")
 
-def convert_to_audio():
+
+def convert_to_audio(youtube_url, track_name):
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -11,11 +16,27 @@ def convert_to_audio():
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'outtmpl': 'audiofile.mp3'
+        'outtmpl': f'{track_name}.mp3'
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url_entry.get()])
+        ydl.download(youtube_url)
+        messagebox.showinfo(title='Download Success!', message=f'File Saved as {track_name}.mp3')
+
+
+def button_clicked():
+    try:
+        youtube_url = url_entry.get()
+    except Exception as e:
+        messagebox.showerror(title='URL Not Found!', message='URL not found. Please enter valid YouTube URL')
+    else:
+        track_name = file_name_entry.get()
+        if track_name == '':
+            track_name = f'{formatted_time}'
+
+        url_entry.delete(0, END)
+        file_name_entry.delete(0, END)
+        convert_to_audio(youtube_url, track_name)
 
 
 # Create root window
@@ -42,7 +63,7 @@ file_name_label.pack(side=TOP)
 file_name_entry = Entry(root, width=50)
 file_name_entry.pack(side=TOP)
 
-button = Button(root, text="Convert", command=convert_to_audio, width=20)
+button = Button(root, text="Convert", command=button_clicked, width=20)
 button.pack(side=TOP)
 
 root.mainloop()
